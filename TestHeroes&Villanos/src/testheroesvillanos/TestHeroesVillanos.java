6f7287f386569acb557bec4c447768672b0c9601
Unknown <yaki3011@gmail.com>
2018-03-10 08:54:05 -0300

    edir main

--- TestHeroes&Villanos/src/testheroesvillanos/TestHeroesVillanos.java
+++ TestHeroes&Villanos/src/testheroesvillanos/TestHeroesVillanos.java
@@ -1,17 +1,24 @@
-
 package testheroesvillanos;
 
 /**
  * @author Yaki's
  */
-
 public class TestHeroesVillanos {
 
-    
     public static void main(String[] args) {
-     
 
+        Enfrentable dosFantasticos = new Liga("Los Dos Fantásticos");
+        Enfrentable mole = new Personaje("Mole", 4);
+        Enfrentable hombreInvisible = new Personaje("El Hombre Invisible", 3);
 
+        dosFantasticos.addEnfrentable(mole);
+        dosFantasticos.addEnfrentable(hombreInvisible);
+
+        Enfrentable ligaJusticia = new Liga("Liga de la Justicia");
+        Enfrentable batman = new Personaje("Batman", 10);
+
+        ligaJusticia.addEnfrentable(dosFantasticos);
+        ligaJusticia.addEnfrentable(batman);
 
     }
 
