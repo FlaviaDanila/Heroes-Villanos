6a3e84c33ef5895dff2504416e8efb239e963119
maxi2693 <maximiliano_2693@hotmail.com>
2018-03-15 17:42:33 -0300

    holaaa

--- TestHeroes&Villanos/src/testheroesvillanos/TestHeroesVillanos.java
+++ TestHeroes&Villanos/src/testheroesvillanos/TestHeroesVillanos.java
@@ -39,6 +39,7 @@ public class TestHeroesVillanos {
         //basta chicos, me fui!
         //FRAN <3
         //Desde mi compu anduvo la consola ._. jaja
+        // Bueno a ver si le anda al PANA ahora??? :P
     }
 
 }
