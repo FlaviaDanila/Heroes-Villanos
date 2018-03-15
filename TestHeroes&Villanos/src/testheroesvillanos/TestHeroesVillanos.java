84c82dd1ee4ef99f6af316b10a3eb39c37f4e9be
Flavia Danila Yakimiuk <yaki3011@gmail.com>
2018-03-15 10:34:06 -0300

    borrando

--- TestHeroes&Villanos/src/testheroesvillanos/TestHeroesVillanos.java
+++ TestHeroes&Villanos/src/testheroesvillanos/TestHeroesVillanos.java
@@ -23,20 +23,14 @@ public class TestHeroesVillanos {
         
         //aca estuvo juanito
         //Nare estuvo aquí
-        //NareLovers ♥ ahre
-        
+        //NareLovers ♥ ahre    
         //Nicolás //
-
         //juan2
         //que alguien arregle este programa ;(
         //Antes que Fran y el Pana, yo!
-<<<<<<< HEAD
-        
         //Nicolás 2//
-=======
         //Todos antes que ellos, tardan mil años xD
         //les falta mucho?
->>>>>>> master
     }
 
 }
